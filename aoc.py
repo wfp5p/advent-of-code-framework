7d60fc291fcc815b35986aac487a5a4ac346b52e
Bill Pemberton <wfp5p@worldbroken.com>
2025-11-25 12:45:18 -0500

    ruff reformat
    formatting only, no code changes

--- aoc.py
+++ aoc.py
@@ -93,54 +93,69 @@ def runDay(args):
 
 def main():
     """main"""
-    # fmt: off
+
     argp = argparse.ArgumentParser(description='Advent of Code templater and runner')
-    argp.add_argument('--day',
-                      help='day to start [1..25]',
-                      type=int_arg_range(mini=0, maxi=25),
-                      default=0)
-    argp.add_argument('--year',
-                      help='year of challenge',
-                      type=int_arg_range(mini=2015, maxi=2030),
-                      default=config.DEFAULT_YEAR)
+    argp.add_argument(
+        '--day',
+        help='day to start [1..25]',
+        type=int_arg_range(mini=0, maxi=25),
+        default=0,
+    )
+    argp.add_argument(
+        '--year',
+        help='year of challenge',
+        type=int_arg_range(mini=2015, maxi=2030),
+        default=config.DEFAULT_YEAR,
+    )
 
     argp_subs = argp.add_subparsers(dest='command', required=True)
 
     # new command with no options
     argp_new = argp_subs.add_parser('new', help='create a template for a new day')
-    argp_new.add_argument('--baseclass',
-                          help='baseclass to use for Solution class',
-                          default='StrSplitSolution',
-                          choices=['TextSolution',
-                                   'IntSolution',
-                                   'StrSplitSolution',
-                                   'IntSplitSolution'])
+    argp_new.add_argument(
+        '--baseclass',
+        help='baseclass to use for Solution class',
+        default='StrSplitSolution',
+        choices=['TextSolution', 'IntSolution', 'StrSplitSolution', 'IntSplitSolution'],
+    )
 
     # run command and options
     argp_run = argp_subs.add_parser('run', help='run a days code')
-    argp_run.add_argument('-t', '--test-data',
-                          nargs='?',
-                          const='input.test.txt',
-                          help='run test data (default: intput.test.txt')
-    argp_run.add_argument('--solution', '--solution-module',
-                          default='solution',
-                          help='name of solution module with the .py (default: solution)',
-                          dest='solution_module'
-                          )
-    argp_run.add_argument('--debug',
-                          action='store_true',
-                          help='prints normally-hidden debugging statements')
-    argp_run.add_argument('--profile',
-                          action='store_true',
-                          help='run solution through a performance profiler')
-    argp_run.add_argument('--slow',
-                          action='store_true',
-                          help='specify that long-running solutions (or those requiring manual input) should be run')
-    argp_run.add_argument('--time',
-                          action='store_true',
-                          help='Print information about how long the solution (both parts) took to run')
+    argp_run.add_argument(
+        '-t',
+        '--test-data',
+        nargs='?',
+        const='input.test.txt',
+        help='run test data (default: intput.test.txt',
+    )
+    argp_run.add_argument(
+        '--solution',
+        '--solution-module',
+        default='solution',
+        help='name of solution module with the .py (default: solution)',
+        dest='solution_module',
+    )
+    argp_run.add_argument(
+        '--debug',
+        action='store_true',
+        help='prints normally-hidden debugging statements',
+    )
+    argp_run.add_argument(
+        '--profile',
+        action='store_true',
+        help='run solution through a performance profiler',
+    )
+    argp_run.add_argument(
+        '--slow',
+        action='store_true',
+        help='specify that long-running solutions (or those requiring manual input) should be run',
+    )
+    argp_run.add_argument(
+        '--time',
+        action='store_true',
+        help='Print information about how long the solution (both parts) took to run',
+    )
     args = argp.parse_args()
-    # fmt: on
 
     match args.command:
         case 'run':
